d2d304a0e820cb7694c227355567d103c45359ce
Pedro <pedro1013v@gmail.com>
2025-09-17 23:43:33 -0300

    deleted/changed files

--- Executar.js
+++
@@ -1,9 +0,0 @@
-function Executar(){
-  let nome = document.getElementById("nome").value;
-  let nota1 = Number(document.getElementById("n1").value);
-  let nota2 = Number(document.getElementById("n2").value);
-  let aluno = new Aluno(nome, nota1, nota2);
-  aluno.CalcularMedia();
-  aluno.ExibirMedia();
-  aluno.ExibirSituacao();
-}

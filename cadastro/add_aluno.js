027a8c39c2d551d8a0304577c65ea57e62c10803
Pedro <pedro1013v@gmail.com>
2025-09-22 01:35:16 -0300

    merged filles for better logistics

--- cadastro/add_aluno.js
+++ cadastro/add_aluno.js
@@ -4,6 +4,15 @@ function AdicionarAluno(){
     alunos.push(aluno);
     console.log(alunos);
 }
+function NovaTurma(){
+    let codigo = Math.floor(10000 + Math.random() * 90000);
+    let professor = document.getElementById("professor").value;
+    let turma = new Turma(codigo, professor);
+    turma.ExibirTurma();
+    for(let i=0;i<alunos.length;i++){
+        console.log(alunos[i]);
+    }
+}
 function ResetarAlunos(){
     alunos.length = 0;
 }

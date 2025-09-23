6943b22a1943cbbf96da8cf8c001672113b76614
Pedro <pedro1013v@gmail.com>
2025-09-23 18:23:00 -0300

    run new functions

--- exibicao/exibir.js
+++ exibicao/exibir.js
@@ -3,8 +3,7 @@ function Executar(){
   let nota1 = Number(document.getElementById("n1").value);
   let nota2 = Number(document.getElementById("n2").value);
   let alunoclass = new Aluno(nome, nota1, nota2);
-
   alunoclass.CalcularMedia();
-  alunoclass.ExibirMedia();
-  alunoclass.ExibirSituacao();
+  alunoclass.ResultadoFinal();
+  
 }
